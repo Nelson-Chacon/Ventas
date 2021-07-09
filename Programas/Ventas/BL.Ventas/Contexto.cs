290f4cba99e9ff0da02a34faf787bf1eef746b6c
Nelson Chacon <cursodecomputacion.net@gmail.com>
2021-07-08 21:46:23 -0600

    Agregando parte 4

---
+++ Programas/Ventas/BL.Ventas/Contexto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Ventas
+{
+    public class Contexto: DbContext
+    {
+
+        public Contexto() : base(@"Data Source=(LocalDb)\MSSQLLocalDB;AttachDBFilename=" +
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\TiendaDeRopa.mdf")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            
+        }
+
+        public DbSet<Niño> Niños { get; set; }
+    }
+}
---
+++ Programas/Ventas/Win.Ventas/FormMenu.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Ventas
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void loginToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            login();
+        }
+
+        private void login()
+        {
+            var formLogin = new FormLogin();
+            formLogin.ShowDialog();
+        }
+
+        private void departamentoDeNiñosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formnino = new FormNiño();
+            formnino.MdiParent = this;
+            formnino.Show();
+        }
+
+        private void departamentoDeMujerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formMujer = new FormMujer();
+            formMujer.MdiParent = this;
+            formMujer.Show();
+        }
+
+        private void departamentoDeHombreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formHombre = new FormHombre();
+            formHombre.MdiParent = this;
+            formHombre.Show();
+        }
+
+        private void FormMenu_Load(object sender, EventArgs e)
+        {
+            login();
+        }
+    }
+}
